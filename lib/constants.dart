640f7658cead1deaf596e229bd1474077096e5fa
Jamel <jamelbd97@gmail.com>
2022-04-15 04:51:01 +0100

    url

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
 
 class Constants {
-  static const serverUrl = "https://kitebi.herokuapp.com/";
+  //static const serverUrl = "https://kitebi.herokuapp.com/";
+  static const serverUrl = "http://localhost:5000/";
   static const bookDataUrl = serverUrl + "books/";
   static const audiobookDataUrl = serverUrl + "audiobooks/";
   static const userDataUrl = serverUrl + "users/";
